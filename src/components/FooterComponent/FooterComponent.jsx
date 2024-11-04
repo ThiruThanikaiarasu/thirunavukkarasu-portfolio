beed1a036cdc992f0d1d6b5d7eb0cab75702c43e
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-11-05 01:47:26 +0530

    UPDATE - Update the footer with github and linkedin icons.

--- src/components/FooterComponent/FooterComponent.jsx
+++ src/components/FooterComponent/FooterComponent.jsx
@@ -1,12 +1,25 @@
 import { Linkedin } from 'lucide-react'
 import React from 'react'
 
+import github_dark from '../../assets/images/github_dark.svg'
+import github_light from '../../assets/images/github_light.svg'
+import linkedin_dark from '../../assets/images/linkedin_dark.svg'
+import linkedin_light from '../../assets/images/linkedin_light.svg'
+
+import useDataContext from '../../hooks/useDataContext'
+
 const FooterComponent = () => {
+
+    const { isDarkMode } = useDataContext()
+
+    const linkedInIcon = isDarkMode ? linkedin_light : linkedin_dark
+    const githubIcon = isDarkMode ? github_light : github_dark
+
     return (
         <footer
             className="
                         py-6 px-6
-                        flex justify-between
+                        flex justify-start
                         bg-[#F6F6F6] dark:bg-[#2C2C2C]
                         rounded-lg
                     "
@@ -37,19 +50,12 @@ const FooterComponent = () => {
             </div>
             
             <div
-                className="
-                            p-2
-                            bg-[#FFFFFF]
-                            rounded-full
-                            border
-                            hover:ring-4 hover:ring-[#eaeaea] dark:hover:ring-[#252525]
-                        "
+                className="flex justify-evenly items-center w-6 h-6 ml-72"
             >
-                <Linkedin 
-                    fill='black'
-                    size={18}
-                />
+                <img src={linkedInIcon} alt="" className="mr-2 mt-2"/>
+                <img src={githubIcon} alt="" className="ml-4 mt-2"/>
             </div>
+
         </footer>
     )
 }
